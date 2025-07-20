5f9e407abd85927b1cc6e2600b2f23fca299e3f3
Samuel <samuelrivba@gmail.com>
2025-07-20 17:08:31 -0500

    Add Firebase integration and sign in

---
+++ src/routes/sign-in/sign-in.component.tsx
@@ -0,0 +1,24 @@
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils.ts";
+
+type Props = {};
+const SignIn = ({}: Props) => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth({
+      uid: user.uid,
+      displayName: user.displayName || undefined,
+      email: user.email || undefined,
+    });
+  };
+
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    </div>
+  );
+};
+export default SignIn;
---
+++ src/utils/firebase/firebase.utils.ts
@@ -0,0 +1,60 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPopup,
+  signInWithRedirect,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_apiKey || "",
+  authDomain: import.meta.env.VITE_authDomain || "",
+  projectId: import.meta.env.VITE_projectId || "",
+  storageBucket: import.meta.env.VITE_storageBucket || "",
+  messagingSenderId: import.meta.env.VITE_messagingSenderId || "",
+  appId: import.meta.env.VITE_appId || "",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, provider);
+
+export const db = getFirestore(app);
+
+export const createUserDocumentFromAuth = async (userAuth: {
+  uid?: string;
+  displayName?: string;
+  email?: string;
+}) => {
+  if (!userAuth || !userAuth.uid) return;
+
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.error("Error creating the user", error);
+    }
+  }
+
+  return userDocRef;
+};
