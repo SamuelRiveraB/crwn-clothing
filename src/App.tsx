8c33e05c5b19adfe12c4f6497a316845e39a5c7e
Samuel <samuelrivba@gmail.com>
2025-07-20 15:11:58 -0500

    Add Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      {/* Add more routes as needed */}
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 };
